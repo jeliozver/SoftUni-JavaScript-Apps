48fd61a9ba830c2566811427f41f2a52012e7dd4
Jeliozver <jelioradoev@gmail.com>
2018-03-24 08:26:00 +0200

    refactored code

--- 02-Async-Programming-and-Promises-Lab/P02-Blog/attachEvents.js
+++ 02-Async-Programming-and-Promises-Lab/P02-Blog/attachEvents.js
@@ -8,56 +8,50 @@ function attachEvents() {
 
     // Supply appkey, username and password from your Kinvey project
     // You can import sample entries from posts.json and comments.json files
-    const appKey = '';
+    const appKey = ``;
     const username = '';
     const password = '';
 
     const baseUrl = `https://baas.kinvey.com/appdata/${appKey}/`;
     const auth = {'Authorization': 'Basic ' + btoa(username + ':' + password)};
 
-    const getPostsReq = {
-        method: 'GET',
-        url: baseUrl + 'posts',
-        headers: auth,
-        success: displayPosts,
-        error: handleError
-    };
-
-    const getPostByIdReq = {
-        method: 'GET',
-        url: '',
-        headers: auth,
-        success: displayPost,
-        error: handleError
-    };
-
-    const getPostComments = {
-        method: 'GET',
-        url: '',
-        headers: auth,
-        success: displayComments,
-        error: handleError
-    };
+    function request(endpoint) {
+        return $.ajax({
+            method: 'GET',
+            url: baseUrl + endpoint,
+            headers: auth
+        });
+    }
 
     loadBtn.on('click', loadPosts);
     viewBtn.on('click', viewPost);
 
     function loadPosts() {
-        $.ajax(getPostsReq);
+        request('posts')
+            .then(displayPosts)
+            .catch(handleError);
     }
 
     function viewPost() {
-        let id = posts.find('option:selected').attr('value');
-        getPostByIdReq.url = baseUrl + 'posts/' + id;
-        getPostComments.url = baseUrl + 'comments/?query=' + `{"post_id":"${id}"}`;
-        $.ajax(getPostByIdReq);
-        $.ajax(getPostComments);
+        let id = posts
+            .find('option:selected')
+            .attr('value');
+
+        request(`posts/${id}`)
+            .then(displayPost)
+            .catch(handleError);
+
+        request(`comments/?query={"post_id":"${id}"}`)
+            .then(displayComments)
+            .catch(handleError);
     }
 
     function displayPosts(res) {
         posts.empty();
         for (let post of res) {
-            posts.append($('<option>').attr('value', `${post._id}`).text(`${post.title}`));
+            posts.append($('<option>')
+                .attr('value', `${post._id}`)
+                .text(`${post.title}`));
         }
     }
 
